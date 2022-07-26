75d8c0c672531a14843fdd53618f1de1d22812bf
kirino-k <kirino-k@ncchd.go.jp>
2022-07-26 12:34:58 +0900

    modify output layout

--- src/create_master.sh
+++ src/create_master.sh
@@ -3,13 +3,15 @@
 DIR_SRC=$(cd $(dirname $0); pwd)
 DIR_OUT=${DIR_SRC}/out
 
+echo 'コード1,分類1,コード2,分類2,薬効分類コード,薬効分類名,薬効分類番号,薬効分類細目名' > ${DIR_OUT}/output.csv
+
 curl https://www.kegg.jp/brite/jp08301 |
   grep -A 11 'env' |
   cat - <(cat ${DIR_SRC}/additional.js) |
   node |
   sed -E 's/^<b>([0-9]{1})(.*)<\/b>(,[0-9]{2})(.*)(,[0-9]{3})(.*)(,[0-9]{4})(.*)$/\1,\2\3,\4\5,\6\7,\8/' |
   sed -E 's/；.*$//' |
-  tr -d ' ' > ${DIR_OUT}/output.csv
+  tr -d ' ' >> ${DIR_OUT}/output.csv
 
 cat ${DIR_OUT}/output.csv |
   nkf -s > ${DIR_OUT}/薬効分類.csv
